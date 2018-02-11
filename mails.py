28dcf140ad36611f3525d60575a1258c4d7912c6
yuesj <479100885@qq.com>
2018-02-12 01:27:55 +0800

    '去除依赖，自带压缩功能'

--- mails.py
+++ mails.py
@@ -8,6 +8,7 @@
 """
 import smtplib
 import os
+import zipfile
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
@@ -56,17 +57,12 @@ class Mail:
             file_list = [x for x in path_list if not os.path.isdir(x)]
             for file in file_list:
                 msg.attach(self._attach_file(file))
-            if len(dir_list) > 0:
-                try:
-                    import filezip
-                    for dirs in dir_list:
-                        dir_name = os.path.basename(dirs.rstrip('/'))
-                        dir_name_zip = dir_name + '.zip'
-                        filezip.filezip(dirs, dir_name_zip)
-                        msg.attach(self._attach_file(dir_name_zip))
-                        os.remove(dir_name_zip)
-                except Exception as e:
-                    print(e, '文件压缩模块未加载,请下载同步下filezip.py，将跳过目录')
+            for dirs in dir_list:
+                dir_name = os.path.basename(dirs.rstrip('/'))
+                dir_name_zip = dir_name + '.zip'
+                self.filezip(dirs, dir_name_zip)
+                msg.attach(self._attach_file(dir_name_zip))
+                os.remove(dir_name_zip)
 
         # 发送
         try:
@@ -102,9 +98,57 @@ class Mail:
             path_list = path.replace(';', ',').split(',')
             return [x.strip() for x in path_list]
 
+    def filezip(self, path: str='输入文件', file_path: str='压缩结果文件'):
+        """
+        压缩目录，或文件
+        :param path: 多个文件，目录的路径，以";"或","分割
+        :param file_path: 压缩结果文件
+        :return:
+        """
+        if not (file_path and file_path.endswith('zip')):
+            raise InterruptedError('请输入压缩结果文件:*.zip')
+        zp = zipfile.ZipFile(file_path, 'w', zipfile.ZIP_DEFLATED)
+        zpfile = dict()
+        if path:
+            path_list = [x.strip() for x in path.replace(";", ",").split(",")]
+            for ph in path_list:
+                if not os.path.exists(ph):
+                    print('%s不存在，跳过该文件' % ph)
+                else:
+                    if os.path.isfile(ph):
+                        zpfile[ph] = os.path.basename(ph)
+                    else:
+                        # 变目录的绝对路径为相对路径
+                        parent_path = os.path.split(ph.rstrip('/'))[0]
+                        for file in self.dir_list(ph):
+                            zpfile[file] = file.replace(parent_path, '')
+        for path, filename in zpfile.items():
+            zp.write(path, filename)
+            print('add %s' % path)
+        zp.close()
+        return file_path
+
+    @staticmethod
+    def dir_list(path_dir):
+        file_list = list()
+
+        def listdir(path):
+            """
+            遍历目录
+            :param path:
+            :return:
+            """
+            for ph in os.walk(path):
+                # 文件
+                for file in ph[2]:
+                    file_list.append(os.path.join(ph[0], file).replace('\\', '/'))
+                for dirs in ph[1]:
+                    listdir(os.path.join(ph[0], dirs).replace('\\', '/'))
+            return file_list
+        return listdir(path_dir)
 
 if __name__ == '__main__':
     a = Mail()
-    s = a.send('201519832@qq.com', '你哈', '主题', 'content', 'a.ini,b.ini,log,' + r'D:\IDM')
+    s = a.send('201519832@qq.com', '你哈', '主题', '不错呀。小伙子', 'a.ini,b.ini,log,' + r'D:\IDM')
     if s:
         print('发送成功')
